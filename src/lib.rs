bbad319db1836c0d4db5f5e95306056b8612de9d
Floris Cornel <floris.cornel@colorkrew.com>
2022-12-14 05:33:28 +0900

    new cargo project

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+
+use std::fs::File;
+use std::io::{self, BufRead};
+
+pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let mut path: String = "inputs/".to_owned();
+    path.push_str(filename);
+    let file = File::open(path)?;
+    Ok(io::BufReader::new(file).lines())
+}
